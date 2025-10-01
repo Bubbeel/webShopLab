f2ecd9cae010a7a000eb2a8fce3033534a23f956
Bubbeel <konradjanik.se@gmail.com>
2025-10-01 12:29:11 +0200

    added the basic database

--- src/main/java/com/kth/DB/DBManager.java
+++ src/main/java/com/kth/DB/DBManager.java
@@ -1,6 +1,41 @@
 package com.kth.DB;
 
-public class DBManager {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/db", loadOnStartup = 1)
+public class DBManager extends HttpServlet {
+    private static DBManager instance = null;
+    private Connection con = null;
+    private boolean yesno = false;
+
+    public static DBManager getInstance(){
+        if(instance == null){
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    public DBManager(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop?user=root&password=Kutas123");
+            System.out.println("Successful ig");
+            yesno = true;
+        } catch (Exception e) {
+            System.out.println("Failed");
+            yesno = false;
+            e.printStackTrace();
+        }
+    }
 
 }
